243114a288a72c51e2b8b984e602c1002c04cd2e
Shailesh Gavathe <sgavathe@gmail.com>
2018-07-25 16:33:34 -0400

    source code upload
    source code upload

---
+++ BDD-TDD-IN-ONE/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD_TDD_IN_ONE
+{
+   
+    public class Calculator  
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Result { get; set; }
+
+        public int Add()
+        {
+            Result = FirstNumber + SecondNumber;
+            return Result;
+        }
+
+        
+    }
+   
+}
---
+++ BDD-TDD-IN-ONE/singletondriver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace BDD_TDD_IN_ONE
+{
+    public class singletondriver
+    {
+        public static IWebDriver driver = new ChromeDriver();        
+
+    }
+}
